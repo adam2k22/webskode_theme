ebaf7f6ae42124115938f8cf9752ae1ef7d56588
adam2k22 <102650984+adam2k22@users.noreply.github.com>
2024-07-16 01:27:04 +0530

    Update header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-style-mode" content="1"> <!-- 0 == light, 1 == dark -->
 
-    <title>Best Value Web Development Services in India</title>
+    <title>Webskode || Best Value Web Development Services</title>
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo $logoimg;?>">
     <!-- CSS ============================================ -->
